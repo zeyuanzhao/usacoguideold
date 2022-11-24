b309c036ec737de12bd65b4e73ce5e67a1f64641
tenderpork <14340342+zeyuanzhao@users.noreply.github.com>
2022-11-24 01:54:14 +0000

    traffic

--- traffic/traffic.cpp
+++ traffic/traffic.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdio>
 #include <string>
+#include <vector>
 using namespace std;
 
 int main() {
@@ -10,27 +11,31 @@ int main() {
     int n;
     cin >> n;
 
-    int a = 0, b = 1000;
+    vector<string> mv(n);
+    vector<int> xv(n), yv(n);
 
     for (int i = 0; i < n; i++) {
-        string m;
-        int x, y;
-        cin >> m >> x >> y;
+        cin >> mv[i] >> xv[i] >> yv[i];
+    }
+
+    int a = 0, b = 10000;
+    for (int i = n - 1; i >= 0; i--) {
+        string m = mv[i];
+        int x = xv[i], y = yv[i];
         if (m == "none") {a = max(a, x); b = min(b, y);}
-        else if (m == "off") {a -= x; b -= y;}
-        else if (m == "on") {a += x; b += y;}
+        else if (m == "off") {a += x; b += y;}
+        else if (m == "on") {a -= y; b -= x;}
     }
     cout << a << " " << b << endl;
 
-    a = 0, b = 1000;
+    a = 0, b = 10000;
 
     for (int i = 0; i < n; i++) {
-        string m;
-        int x, y;
-        cin >> m >> x >> y;
+        string m = mv[i];
+        int x = xv[i], y = yv[i];
         if (m == "none") {a = max(a, x); b = min(b, y);}
-        else if (m == "off") {a += x; b += y;}
-        else if (m == "on") {a -= x; b -= y;}
+        else if (m == "off") {a -= y; b -= x;}
+        else if (m == "on") {a += x; b += y;}
     }
     cout << a << " " << b << endl;
 }
