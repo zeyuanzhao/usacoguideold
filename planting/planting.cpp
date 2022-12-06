f02a912516bb656541f220f729bcec0db8c1bb7a
tenderpork <14340342+zeyuanzhao@users.noreply.github.com>
2022-12-06 03:04:37 +0000

    planting

--- planting/planting.cpp
+++ planting/planting.cpp
@@ -5,6 +5,8 @@
 using namespace std;
 
 int main() {
+    freopen("planting.in", "r", stdin);
+    freopen("planting.out", "w", stdout);
     int n; cin >> n;
     map<int, int> fields;
     for (int i = 0; i < n - 1; i++) {
