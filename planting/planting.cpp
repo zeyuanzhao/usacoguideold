7855e9e96476bef20b2a1f1213974753c489708e
tenderpork <14340342+zeyuanzhao@users.noreply.github.com>
2022-12-06 03:03:37 +0000

    planting

---
+++ planting/planting.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <map>
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+    map<int, int> fields;
+    for (int i = 0; i < n - 1; i++) {
+        int a, b;
+        cin >> a >> b;
+        fields[a]++;
+        fields[b]++;
+    }
+    int highest = 0;
+    for (auto i : fields) {
+        highest = max(i.second, highest);
+    }
+    cout << highest + 1;
+}
