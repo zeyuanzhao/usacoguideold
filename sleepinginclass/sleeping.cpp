0aadcd7d2c09c35d7f05411d41745241bbf60a6f
tenderpork <14340342+zeyuanzhao@users.noreply.github.com>
2022-12-27 03:45:52 +0000

    sadf

--- sleepinginclass/sleeping.cpp
+++ sleepinginclass/sleeping.cpp
@@ -30,7 +30,7 @@ int main() {
                 }
             }
             if (works) {
-                cout << n - x;
+                cout << n - x << endl;
                 break;
             }
         }
