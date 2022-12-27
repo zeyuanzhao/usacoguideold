91cb2050b1880c847e974bc3c2fe9539710e3364
tenderpork <14340342+zeyuanzhao@users.noreply.github.com>
2022-12-27 03:45:00 +0000

    sleeping

---
+++ sleepinginclass/sleeping.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t; cin >> t;
+    for (int i = 0; i < t; i++) {
+        int n; cin >> n;
+        vector<int> log(n);
+        int total = 0;
+        for (int j = 0; j < n; j++) {
+            int tmp;
+            cin >> tmp;
+            log[j] = tmp;
+            total += tmp;
+        }
+        for (int x = n; x >= 1; x--) {
+            if (total % x != 0) {
+                continue;
+            }
+            int target = (int) total / x;
+            int sum = 0;
+            bool works = true;
+            for (int j = 0; j < n; j++) {
+                sum += log[j];
+                if (sum == target) {
+                    sum = 0;
+                } else if (sum > target) {
+                    works = false;
+                    break;
+                }
+            }
+            if (works) {
+                cout << n - x;
+                break;
+            }
+        }
+    }
+}
