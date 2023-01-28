3b1768a598bbd35d078bd6bcdf25cc3499d76ec1
tenderpork <14340342+zeyuanzhao@users.noreply.github.com>
2023-01-28 15:57:42 +0000

    lost cow

---
+++ lostcow/lostcow2.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x, y;
+    cin >> x >> y;
+    int total = 0;
+    int pos = x;
+    int distance = 0;
+    while (true) {
+        if (x - y >= 0 && y - pos >= 0 || y - x >= 0 && pos - y >= 0) {
+            total += abs(x - y);
+            break;
+        } else {
+            total += 2 * abs(pos - x);
+        }
+        if (distance) distance *= -2;
+        else distance = 1;
+        pos = x + distance;
+    }
+    cout << total;
+}
