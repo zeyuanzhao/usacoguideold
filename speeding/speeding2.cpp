7c2ac23bd2b4d493a50fbc6f6ae6175a034888b4
tenderpork <14340342+zeyuanzhao@users.noreply.github.com>
2023-01-28 00:39:34 +0000

    speeding

---
+++ speeding/speeding2.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    freopen("speeding.in", "r", stdin);
+    freopen("speeding.out", "w", stdout);
+    int n, m; cin >> n >> m;
+    vector<int> road;
+    for (int i = 0; i < n; i++) {
+        int length, speed;
+        cin >> length >> speed;
+        for (int j = 0; j < length; j++) {
+            road.push_back(speed);
+        }
+    }
+
+    int t = 0;
+    int ans = 0;
+    for (int i = 0; i < m; i++) {
+        int length, speed;
+        cin >> length >> speed;
+        for (int j = 0; j < length; j++) {
+            ans = max(ans, road[t] - speed);
+            t++;
+        }
+    }
+    cout << ans;
+}
