bcc52c97a53233413ddd0f8bcf82d042d0adcd28
tenderpork <14340342+zeyuanzhao@users.noreply.github.com>
2023-01-30 23:47:56 +0000

    blist

---
+++ blist/blist2.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n; cin >> n;
+    vector<int> buckets(1000);
+    for (int i = 0; i < n; i++) {
+        int s, t, b;
+        cin >> s >> t >> b;
+        for (int j = s-1; j < t; j++) {
+            buckets[j] += b;
+        }
+    }
+    cout << *max_element(buckets.begin(), buckets.end());
+}
